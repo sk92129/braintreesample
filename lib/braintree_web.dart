7c3b745583569acc5540460d84609243676d9be2
Sean Kang <skang@geo-blue.com>
2024-09-17 10:55:05 -0700

    init

---
+++ lib/braintree_web.dart
@@ -0,0 +1,107 @@
+/*
+ *  ====================================================
+ *  Copyright (c) 2021. Daniel Nazarian
+ *
+ *  Do not use, edit or distribute without explicit permission.
+ *  Questions, comments or concerns -> email dnaz@danielnazarian.com
+ * ======================================================
+ */
+
+// coverage:ignore-file_
+@JS()
+library braintree_payment;
+
+// ignore: avoid_web_libraries_in_flutter
+import 'dart:html' as html;
+//import 'dart:js_interop'; 
+import 'dart:ui' as ui;
+
+import 'package:flutter/material.dart';
+import 'package:flutter_braintree/flutter_braintree.dart';
+// ignore: implementation_imports
+import 'package:flutter_braintree/src/request.dart';
+import 'package:js/js.dart';
+import 'package:js/js_util.dart';
+
+
+import 'dart:ui_web' as ui_web; // New import for all web-only stuff...
+
+
+
+// EXTERNAL JAVASCRIPT ==========================================
+
+@JS()
+external void initBraintree(auth);
+
+@JS()
+external payment(String auth);
+
+// ==============================================================
+// FLUTTER BRAINTREE WEB ========================================
+// ==============================================================
+
+class BraintreeWidget extends StatefulWidget {
+  const BraintreeWidget();
+
+  @override
+  _BraintreeWidgetState createState() => _BraintreeWidgetState();
+
+  Future<BraintreeDropInResult?> start(BuildContext context, BraintreeDropInRequest request) async {
+    // create div with html embedded
+    String htmlL = """<div id="checkout-message"></div>
+        <div id="dropin-container"></div>
+    <button id="submit-button">Submit payment</button>""";
+    var paymentDiv = html.DivElement()..appendHtml(htmlL);
+
+    // attach to payment container
+    
+    ui_web.PlatformViewRegistry().registerViewFactory('braintree-container', (int viewId) => paymentDiv);
+
+    // show dialog
+    // ignore: unused_local_variable
+    Future dialogResponse = await showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return SimpleDialog(
+          title: const Text('Select assignment'),
+          children: <Widget>[
+            Container(child: this),
+          ]
+        );
+      }
+    );
+    
+    // call js function
+    var promise = payment(request.clientToken ?? "");
+    String? nonce = await promiseToFuture(promise);
+
+    // close dialog
+    Navigator.pop(
+      context,
+    );
+
+    // return nonce
+    if (nonce != null) {
+      BraintreePaymentMethodNonce btNonce = BraintreePaymentMethodNonce(nonce: nonce, typeLabel: 'Visa', description: 'Visa ending in', isDefault: false);
+      return BraintreeDropInResult(paymentMethodNonce: btNonce, deviceData: null);
+    } else {
+      // DIALOG CLOSED OR NONCE INVALID
+      return null;
+    }
+  }
+}
+
+class _BraintreeWidgetState extends State<BraintreeWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return const SingleChildScrollView(
+      child: SizedBox(
+        width: 600.0,
+        height: 300.0,
+        child: HtmlElementView(
+          viewType: 'braintree-container',
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_braintree/flutter_braintree.dart';
+import 'package:sample/braintree_main.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String clientToken = "TEST";
+
+  String getTotal() {
+    return "1.00";
+  }
+
+  bool isWeb() {
+    return kIsWeb;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Flutter / JS Demo"),
+      ),
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Flutter/JS Tutorial',
+              
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => showDialogBraintree(),
+        tooltip: 'Increment',
+        child: const Icon(Icons.attach_money),
+      ),
+    );
+  }
+
+  showDialogBraintree() async {
+        BraintreeDropInRequest request = BraintreeDropInRequest(
+          clientToken: clientToken,
+          collectDeviceData: true,
+          venmoEnabled: true,
+          cardEnabled: true,
+          amount: getTotal(),
+          googlePaymentRequest: BraintreeGooglePaymentRequest(
+            currencyCode: 'USD',
+            billingAddressRequired: false,
+            totalPrice: getTotal(),
+          ),
+        );
+        // ignore: unused_local_variable
+        BraintreeDropInResult? braintreeResult;
+        if (isWeb()) {
+          // WEB VERSION =================================/
+          braintreeResult = await BraintreeWidget().start(context, request);
+        } else {
+          // MOBILE VERSION ===================================/
+          braintreeResult = await BraintreeDropIn.start(request);
+        }
+  }
+}
